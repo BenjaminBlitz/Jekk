c5924bc06c0f67024c61ed24bead8eb0ffea1022
Boubou1611 <nathanaelbosquetlefebvre@yahoo.fr>
2022-05-10 10:25:40 +0200

    L'ennemy en cube qui bouge bien la

---
+++ Assets/Animation mob/EnemyMover.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player.transform);
+        transform.position += transform.forward * 5f * Time.deltaTime;
+        transform.Rotate(1, 270, 1);
+    }
+}
