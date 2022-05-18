c4b00d8abca2b848193762549d844b7bff9d9253
barsollh <hugo.barsolle@edu.esiee.fr>
2022-05-18 15:17:49 +0200

    liaison des scenes (retry)

--- Assets/HUD and Panels/MainMenu.cs
+++ Assets/HUD and Panels/MainMenu.cs
@@ -7,7 +7,8 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Map du jeu", LoadSceneMode.Additive);
+       // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
