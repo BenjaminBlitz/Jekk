cacc931edbd17dc1827eb1f9dd51d38f2c6cbcd0
barsollh <hugo.barsolle@edu.esiee.fr>
2022-05-18 15:27:06 +0200

    retry sans map du jeu

--- Assets/HUD and Panels/MainMenu.cs
+++ Assets/HUD and Panels/MainMenu.cs
@@ -7,8 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("Map du jeu", LoadSceneMode.Additive);
-       // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
