346404a1199328958c836c09488e2f1f4cc2dde8
Boubou1611 <nathanaelbosquetlefebvre@yahoo.fr>
2022-05-19 17:33:05 +0200

    Ajout des items interacrtif!

---
+++ Assets/Scripts/ObjectManager/Interactable.cs
@@ -0,0 +1,40 @@
+  using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float radius = 3f;
+
+    bool isFocus = false;
+    public GameObject player;
+
+    bool hasInteracted = false;
+
+    public virtual void Interact()
+    {
+        /*Debug.Log("INTERACT");*/
+    }
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+        Debug.Log(distance.ToString());
+        if(distance <= radius)
+        {
+            if (!hasInteracted)
+            {
+                Interact();
+                hasInteracted = true;
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);
+
+    }
+
+
+}
+
---
+++ Assets/Scripts/ObjectManager/ItemPickUp.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class ItemPickUp : Interactable
+{
+    public override void Interact()
+    {
+        base.Interact();
+
+        PickUp();
+    }
+
+    void PickUp()
+    {
+        Debug.Log("Picking up Item");
+        Destroy(gameObject);
+    }
+
+
+}
+
