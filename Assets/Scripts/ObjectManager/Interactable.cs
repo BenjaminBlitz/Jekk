cfe7052f851c9bfff84e6a6b18d5605e486f9e23
barsollh <hugo.barsolle@edu.esiee.fr>
2022-05-21 23:54:10 +0200

    fix bug son

--- Assets/Scripts/ObjectManager/Interactable.cs
+++ Assets/Scripts/ObjectManager/Interactable.cs
@@ -17,7 +17,7 @@ public class Interactable : MonoBehaviour
     private void Update()
     {
         float distance = Vector3.Distance(transform.position, playerObject.transform.position);
-        Debug.Log(distance.ToString());
+        //Debug.Log(distance.ToString());
         if(distance <= radius)
         {
             if (!hasInteracted)
