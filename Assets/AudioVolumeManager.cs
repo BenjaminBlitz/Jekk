4ba6703722cdf99c22bc4ccc2ddc6d8f1c29b90e
barsollh <hugo.barsolle@edu.esiee.fr>
2022-05-22 12:37:30 +0200

    sound effect slider volume

--- Assets/AudioVolumeManager.cs
+++ Assets/AudioVolumeManager.cs
@@ -7,10 +7,14 @@ public class AudioVolumeManager : MonoBehaviour
 {
     private static readonly string FirstPlay = "FirstPlay"; 
     private static readonly string VolumePref = "VolumePref";
+    private static readonly string SoundEffectPref = "SoundEffectPref";
     private int firstPlayInt;
     public Slider volumeSlider;
+    public Slider soundEffectSlider;
     private float volumeFloat;
+    private float soundEffectFloat;
     public AudioSource musicSource;
+    public AudioSource[] soundEffectSource;
     public AudioClip Music1;
     public static bool isInGame;
 
@@ -26,14 +30,19 @@ public class AudioVolumeManager : MonoBehaviour
         if (firstPlayInt == 0)
         {
             volumeFloat = 0.25f;
+            soundEffectFloat = 0.5f;
             volumeSlider.value = volumeFloat;
+            soundEffectSlider.value = soundEffectFloat;
             PlayerPrefs.SetFloat(VolumePref, volumeFloat);
+            PlayerPrefs.SetFloat(SoundEffectPref, soundEffectFloat);
             PlayerPrefs.SetInt(FirstPlay, -1);
         }
         else
         {
             volumeFloat = PlayerPrefs.GetFloat(VolumePref);
+            soundEffectFloat = PlayerPrefs.GetFloat(SoundEffectPref);
             volumeSlider.value = volumeFloat;
+            soundEffectSlider.value = soundEffectFloat;
         }
     }
 
@@ -50,6 +59,7 @@ public class AudioVolumeManager : MonoBehaviour
     public void SaveSoundSetting()
     {
         PlayerPrefs.SetFloat (VolumePref, volumeSlider.value);
+        PlayerPrefs.SetFloat (SoundEffectPref, soundEffectSlider.value);
     }
 
     private void OnApplicationFocus(bool focus)
@@ -63,5 +73,9 @@ public class AudioVolumeManager : MonoBehaviour
     public void UpdateSound()
     {
         musicSource.volume = volumeSlider.value;
+        for (int i = 0; i < soundEffectSource.Length; i++)
+        {
+            soundEffectSource[i].volume = soundEffectSlider.value;
+        }
     }
 }
