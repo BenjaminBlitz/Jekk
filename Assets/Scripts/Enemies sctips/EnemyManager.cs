50c81b48c8a4cda01b836fbf034568d15f44203d
BenjaminBlitz <benjamin.blitz@edu.esiee.fr>
2022-05-22 17:39:33 +0200

    on prend d déga

--- Assets/Scripts/Enemies sctips/EnemyManager.cs
+++ Assets/Scripts/Enemies sctips/EnemyManager.cs
@@ -5,21 +5,41 @@ using UnityEngine;
 public class EnemyManager : MonoBehaviour
 {
     [SerializeField] public float healthPoints;
+    public GameObject player;
+    public float damages;
     // Start is called before the first frame update
     void Start()
     {
-        
+        damages = 20;
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-    private void OnCollisionEnter(Collision other)
-    {
-        
 
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        player = GameObject.FindWithTag("Player");
+        if (other.CompareTag("Player"))
+        {
+            player.GetComponent<PlayerManagement>().currentHealth -= damages;
+        }
+
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        player = GameObject.FindWithTag("Player");
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            player.GetComponent<PlayerManagement>().TakeDamage(damages);
+            //player.GetComponent<PlayerManagement>().currentHealth -= damages;
+        }
 
     }
 
