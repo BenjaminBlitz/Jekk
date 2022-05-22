ee6b9e25d03fab8fd712c6113abb160dd64918dc
BenjaminBlitz <benjamin.blitz@edu.esiee.fr>
2022-05-22 14:50:55 +0200

    h

--- Assets/Scripts/Player scripts/PlayerManagement.cs
+++ Assets/Scripts/Player scripts/PlayerManagement.cs
@@ -148,6 +148,10 @@ public class PlayerManagement : MonoBehaviour
         {
             GetExperience(20, lvlPlayer);
         }
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            GetExperience(100, lvlPlayer);
+        }
         Vector3 mouseWorldPosition = Vector3.zero;
         Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
         Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
