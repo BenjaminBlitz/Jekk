c967f2296507029a5c9abdc181e0bf0b87b4318c
BenjaminBlitz <benjamin.blitz@edu.esiee.fr>
2022-05-22 22:00:24 +0200

    xp quand tue enemies

--- Assets/Scripts/Player scripts/BulletManager.cs
+++ Assets/Scripts/Player scripts/BulletManager.cs
@@ -43,6 +43,7 @@ public class BulletManager : MonoBehaviour
             {
                 itemDrop.Create(transform.position);
                 Destroy(other.gameObject);
+                playerManager.GetExperience(50, playerManager.lvlPlayer);
                 EnemyManager.enemiesAlive -= 1;
             }
         }
