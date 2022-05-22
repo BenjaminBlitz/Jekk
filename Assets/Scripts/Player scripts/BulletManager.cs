3c65196e16978be8ad507ddcd9f757c4d6553248
Boubou1611 <nathanaelbosquetlefebvre@yahoo.fr>
2022-05-22 17:55:00 +0200

    Items Icons

--- Assets/Scripts/Player scripts/BulletManager.cs
+++ Assets/Scripts/Player scripts/BulletManager.cs
@@ -31,7 +31,7 @@ public class BulletManager : MonoBehaviour
         hasHit= false;
         //enemy = GameObject.FindWithTag("Mob");
         enemy = other.gameObject;
-        bulletDamage = player.GetComponent<PlayerManagement>().damage;
+        bulletDamage = player.GetComponent<PlayerManagement>().damage * GoCrit(player.GetComponent<PlayerManagement>());
 
         if (other.CompareTag("Mob"))
         {
@@ -39,10 +39,20 @@ public class BulletManager : MonoBehaviour
             enemy.GetComponent<EnemyManager>().healthPoints -= bulletDamage;
             if (enemy.GetComponent<EnemyManager>().healthPoints <= 0)
             {
+                itemDrop.Create(other.transform.position);
                 Destroy(other.gameObject);
-                itemDrop.Create(transform.position);
             }
         }
         Destroy(gameObject);
+    }
+
+    private int GoCrit(PlayerManagement player)
+    {
+        int random = Random.Range(0, 100);
+        if(random < player.critic)
+        {
+            return 2;
+        }
+        return 1;
     }
 }
