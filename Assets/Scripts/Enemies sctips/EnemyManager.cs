6464cc2255708dfacde7ab2a78fd2126375f0053
BenjaminBlitz <benjamin.blitz@edu.esiee.fr>
2022-05-22 22:22:15 +0200

    tag

--- Assets/Scripts/Enemies sctips/EnemyManager.cs
+++ Assets/Scripts/Enemies sctips/EnemyManager.cs
@@ -37,10 +37,6 @@ public class EnemyManager : MonoBehaviour
             {
                 this.transform.localScale *= 5.8f / (this.transform.localScale.x / Mathf.Pow(this.transform.localScale.x, 0.7f));
             }
-            else
-            {
-                this.transform.localScale *= 1.28f / (this.transform.localScale.x / Mathf.Pow(this.transform.localScale.x, 0.97f));
-            }
         }
         this.GetComponent<EnemyMover>().Movespeed *= this.transform.localScale.x;
     }
